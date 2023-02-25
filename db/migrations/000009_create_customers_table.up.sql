b3c3bdb6009e79f8c8ae5d17786f23ff69f99495
romankravchuk <kravchuk.roman02@gmail.com>
2023-02-25 21:00:47 +0800

    updated create_customers_table.up.sql

--- db/migrations/000009_create_customers_table.up.sql
+++ db/migrations/000009_create_customers_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE
-    IF NOT EXISTS customers (
+    customers (
         id uuid NOT NULL,
         first_name varchar(100) NOT NULL,
         last_name varchar(100) NOT NULL,
         email varchar(150) NOT NULL,
         phone varchar(50) NOT NULL,
+        passhash varchar(1024) NOT NULL,
         license_number varchar(100) NOT NULL,
         address varchar(100) NOT NULL,
         CONSTRAINT pk_customers PRIMARY KEY (id)
